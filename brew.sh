56d4ca18af739515cff0be533f66b23ced5d562b
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2018-09-18 11:41:45 +0200

    Initial files

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install tools
+brew install git
+brew install node
+brew install fish
