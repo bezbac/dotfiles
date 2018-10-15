e64c25def62ad6cc6554dfa0e0fea8b93dbc1cb7
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2018-10-15 11:24:51 +0200

    Added symlink scipt

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+# Link gitconfig
+ln -s ~/Documents/Dev/dotfiles/.gitconfig ~/.gitconfig
+
+# Link fish configuration
+ln -s ~/Documents/Dev/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+
+# Link atom configuration
+ln -s ~/Documents/Dev/dotfiles/.atom/init.coffee ~/.atom/init.coffee
+ln -s ~/Documents/Dev/dotfiles/.atom/keymap.cson ~/.atom/keymap.cson
+ln -s ~/Documents/Dev/dotfiles/.atom/packages.json ~/.atom/packages.json
+ln -s ~/Documents/Dev/dotfiles/.atom/settings.json ~/.atom/settings.json
+ln -s ~/Documents/Dev/dotfiles/.atom/snippets.json ~/.atom/snippets.json
+ln -s ~/Documents/Dev/dotfiles/.atom/style.less ~/.atom/style.less
