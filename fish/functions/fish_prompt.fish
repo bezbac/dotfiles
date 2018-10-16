310ea94f24abdb599fd9c021aa3ca8f5c4a5e855
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2018-10-16 15:26:14 +0200

    Updated prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,9 @@
+# Colors:
+# Yellow: ffbf00
+# Aquamarine: 7fffd4
+#
+#
+
 function parse_git_branch -d "Parse current Git branch name"
   command git symbolic-ref --short HEAD ^/dev/null;
     or echo (command git show-ref --head -s --abbrev HEAD)[1]
@@ -8,11 +14,15 @@ function parse_current_folder -d "Replace '$HOME' with '~'"
 end
 
 function fish_prompt
+
+  # Clear line
+  printf "\033[K"
+
   # Time
   printf (date "+$c2%H$c0:$c2%M$c0:$c2%S ")
 
   # User
-  set_color red
+  set_color 7fffd4
   printf (whoami)
   printf " "
 
@@ -20,7 +30,7 @@ function fish_prompt
   printf (parse_current_folder)
   printf " "
 
-  set_color green
+  set_color ffbf00
   set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
   if test -n "$is_git_repository"
     printf (parse_git_branch)
