aae934a2d8083fb6a85e776ea6c7e3af15301f84
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2019-06-07 13:49:26 +0200

    Setup aliases

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,31 @@ set -g SPACEFISH_DOTNET_SHOW false
 set -g SPACEFISH_KUBECONTEXT_SHOW false
 set -g SPACEFISH_BATTERY_SHOW false
 
-# Add flutter to PATH
+# PATH
+## Add flutter to PATH
 set PATH /Users/ben/Library/Caches/flutter/bin $PATH .
 set PATH /Users/ben/.cargo/bin $PATH .
+
+## Add git to PATH
+set PATH /usr/local/git/bin $PATH .
+set PATH /usr/local/opt/make/libexec/gnubin $PATH .
+
+# ALIASES
+## Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+## List
+alias la="ls -a"
+
+## Docker
+alias dc="docker-compose"
+
+## Git
+### Pretty git history
+alias glog="git log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'"
+
+alias gd="git diff --stat"
