e801834809fbebc8317c20f6a7e9b2a985ed7a51
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2020-08-22 00:26:32 +0200

    Update homebrew script

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -1,3 +1,9 @@
+### --- CLEANING UP HOMEBREW ---
+### In order to clean up homebrew run
+### brew bundle dump
+### Remove all unwanted packages in the generated Brewfile then and run
+### brew bundle --force cleanup
+
 #!/bin/bash
 
 # Make sure we’re using the latest Homebrew
@@ -12,10 +18,14 @@ brew install git-lfs
 brew install fish
 
 # Install tools
-brew install make
-brew install wget
-brew install htop
-brew install hyperfine
+brew install starship # Fish prompt
+brew install make # Makefile support
+brew install wget # Download
+brew install htop # System dashboard
+brew install thefuck # Correct previous commands
+brew install fzf # Fuzzy finder
+brew install bat # File preview
+brew install ripgrep # Search
 
 # Javascript environment
 brew install node
@@ -26,11 +36,18 @@ brew install rustup
 
 # Python environment
 brew install python
+brew install pyenv
+brew install poetry
 python -m pip install 'python-language-server[all]'
 
 # Vim
 brew install neovim
+pip3 install --user pynvim
 
 # Install apps
 brew cask install iterm2
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install spotify
+brew cask install handbrake
 brew cask install alacritty
