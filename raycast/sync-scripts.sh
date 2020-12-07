58c94a13fccb959dccdea33c0a1989f76a6c1326
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2020-12-07 23:30:58 +0100

    Add raycast scripts

---
+++ raycast/sync-scripts.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+array=(
+    "commands/media/spotify/images/spotify-logo.png"
+    "commands/media/spotify/spotify-next-track.applescript"
+    "commands/media/spotify/spotify-previous-track.applescript"
+    "commands/media/spotify/spotify-play-pause.applescript"
+    "commands/media/spotify/spotify-now-playing-url.applescript"
+
+    "commands/developer-utils/ping.sh"
+
+    "commands/navigation/open-desktop.sh"
+    "commands/navigation/open-documents.sh"
+    "commands/navigation/open-home.sh"
+    "commands/navigation/open-home.sh"
+
+    "commands/dashboard/system-activity.sh"
+
+    "commands/web-searches/npmjs.sh"
+    "commands/web-searches/images/npmjs.png"
+)
+for i in "${array[@]}"
+do
+    dlpath="$i"
+
+    prefixes=(
+        "commands/media/spotify/"
+        "commands/developer-utils/"
+        "commands/navigation/"
+        "commands/dashboard/"
+        "commands/web-searches/"
+    )
+
+    for prefix in "${prefixes[@]}"
+    do
+        dlpath="${dlpath#$prefix}"
+    done
+
+    dlpath="scripts/remote/$dlpath"
+
+	echo "Downloading $i to $dlpath"
+    
+    curl --create-dirs https://raw.githubusercontent.com/raycast/script-commands/master/$i --output $dlpath
+    chmod +x $dlpath
+done
+
+echo 'remote/' > scripts/.gitignore
