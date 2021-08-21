5a4abe07f34ef07eb3dbfae2a4a6c3d21dcd8eda
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-08-21 14:05:42 +0200

    Update raycast scripts

--- raycast/sync-scripts.sh
+++ raycast/sync-scripts.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 
 array=(
-    "commands/media/spotify/images/spotify-logo.png"
-    "commands/media/spotify/spotify-next-track.applescript"
-    "commands/media/spotify/spotify-previous-track.applescript"
-    "commands/media/spotify/spotify-play-pause.applescript"
-    "commands/media/spotify/spotify-now-playing-url.applescript"
-
     "commands/developer-utils/ping.sh"
-
-    "commands/navigation/open-desktop.sh"
-    "commands/navigation/open-documents.sh"
-    "commands/navigation/open-home.sh"
-    "commands/navigation/open-home.sh"
-
-    "commands/dashboard/system-activity.sh"
-
-    "commands/web-searches/npmjs.sh"
-    "commands/web-searches/images/npmjs.png"
-
-    "commands/conversions/hex-to-rgba.sh"
-    "commands/conversions/hex-to-rgb.sh"
 
     "commands/system/sample-colour.swift"
 )
@@ -29,12 +10,7 @@ do
     dlpath="$i"
 
     prefixes=(
-        "commands/media/spotify/"
         "commands/developer-utils/"
-        "commands/navigation/"
-        "commands/dashboard/"
-        "commands/web-searches/"
-        "commands/conversions/"
         "commands/system/"
     )
 
