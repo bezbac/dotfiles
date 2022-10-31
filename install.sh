d3cb91b21a081610980738119585c3fc6f6d0cf9
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2022-10-31 14:21:12 +0100

    Remove vscode snippets from `install.sh`

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ ln -s $DOTFILE_ROOT/.gitconfig $HOME/.gitconfig
 ln -s $DOTFILE_ROOT/nvim $HOME/.config/nvim
 ln -s $DOTFILE_ROOT/starship/starship.toml $HOME/.config/starship.toml
 ln -s $DOTFILE_ROOT/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $DOTFILE_ROOT/vscode/snippets/ $HOME/Library/Application\ Support/Code/User
 ln -s $DOTFILE_ROOT/zsh/.zshrc $HOME/.zshrc
 mkdir $HOME/.sheldon && ln -s $DOTFILE_ROOT/sheldon/plugins.toml $HOME/.sheldon/plugins.toml
 ln -s $DOTFILE_ROOT/bash/.bashrc $HOME/.bashrc
