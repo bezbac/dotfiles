c17e74c4cd0c0f0452834da9ffc288b20d7c6c72
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2023-09-20 08:43:36 +0200

    Update dock applications

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,6 +3,7 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
 dockutil --no-restart --add "/Applications/Figma.app"
 dockutil --no-restart --add "/Applications/Alacritty.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
@@ -10,6 +11,7 @@ dockutil --no-restart --add "/System/Volumes/Preboot/Cryptexes/App/System/Applic
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Linear.app"
+dockutil --no-restart --add "/Applications/Anybox.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/System Settings.app"
 
