da23207c7119ab7f126be4b6bf33b86a1d2b5a8b
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2023-12-22 16:07:04 +0100

    Work on hyperlink rules

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -33,5 +33,34 @@ config.font = wezterm.font {
 config.color_scheme_dirs = { '/Users/ben.bachem/Documents/Dev/dotfiles/wezterm/themes' }
 config.color_scheme = 'concrete'
 
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make superchat's' linear issues clickable
+table.insert(config.hyperlink_rules, {
+  regex = [[\b(?:plat|PLAT)-(\d+)]],
+  format = 'https://linear.app/superchat/issue/PLAT-$1',
+})
+
+-- -- open files in vscode
+-- table.insert(config.hyperlink_rules, {
+--   regex = [[((?:\w+\/?)+(?:\.\w+)+)]],
+--   format = 'vscode://$1',
+-- })
+
+-- wezterm.on('open-uri', function(window, pane, uri)
+-- 	local start, match_end = uri:find('vscode://');
+-- 	if start == 1 then
+-- 		local cwd = pane:get_current_working_dir()
+-- 		local file_path = cwd .. uri:sub(match_end+1)
+
+--     wezterm.log_info(cwd)
+
+-- 		local url = 'vscode://' .. file_path .. '?windowId=_blank'
+
+--     -- prevent the default action from opening in a browser
+--     return url
+-- 	end
+-- end)
+
 -- and finally, return the configuration to wezterm
 return config
