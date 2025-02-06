4cf4a5cb2fcab8dfa6930f99f55d6599ea3e7ef1
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2025-02-06 10:47:50 +0100

    wip: Open files from wezterm in vscode

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
@@ -68,6 +69,34 @@ config.keys = {
   -- Make Option-Right equivalent to Alt-f; forward-word
   { key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"} },
 }
+
+-- Update open uri function to open vscode:// links
+wezterm.on("open-uri", function(window, pane, uri)
+  -- TODO: Use https://wezfurlong.org/wezterm/config/lua/wezterm.procinfo/current_working_dir_for_pid.html instead
+  local success, stdout, stderr = wezterm.run_child_process { 'sh', '-c', 'lsof -a -p $$ -d cwd -Fn | sed -n \'3p\' | sed \'s/^n//\'' }
+
+  -- Logs can be found at: $HOME/.local/share/wezterm
+  print(stdout)
+
+  return true
+
+	-- local start, match_end = uri:find("vscode://")
+	-- if start == 1 then
+	-- 	local cwd = pane:get_current_working_dir()
+	-- 	local file_path = cwd .. uri:sub(match_end + 1)
+	-- 	local url = "vscode://file" .. file_path
+	-- 	window.open_with(url)
+	-- 	return false
+	-- end
+end)
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+table.insert(config.hyperlink_rules, {
+  regex = "[/.A-Za-z0-9_-]+\\.[A-Za-z0-9]+(:\\d+)*(?=\\s*|$)",
+  format = "https://vscode://$0"
+})
 
 -- and finally, return the configuration to wezterm
 return config
