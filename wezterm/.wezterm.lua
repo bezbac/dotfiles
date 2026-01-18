14eaf04f0a611f9bb0eb978b07906cff5057a729
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-01-18 16:42:06 +0100

    Remove unused whitespace from `wezterm/.wezterm.lua`

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -65,7 +65,7 @@ config.keys = {
 
    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
   { key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"} },
-  
+
   -- Make Option-Right equivalent to Alt-f; forward-word
   { key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"} },
 }
@@ -78,7 +78,7 @@ local function find_absolute_file_path(relative_path)
 
   if success then
     local first_line = stdout:match("[^\n]+")
-    if first_line then      
+    if first_line then
       return first_line
     end
   end
