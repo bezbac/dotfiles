4635a6e66d44e584c258c72437d4f66209287a49
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-01-26 14:44:45 +0100

    Verify userscript download with checksum

--- install_userscripts.sh
+++ install_userscripts.sh
@@ -3,5 +3,21 @@
 # Set dotfile root to directory of this script
 DOTFILE_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+download_and_verify_file() {
+    local url=$1
+    local output_path=$2
+    local expected_checksum=$3
+    curl -fsSL "$url" -o "$output_path"
+    local actual_checksum=$(sha256sum "$output_path" | awk '{print $1}')
+    if [ "$actual_checksum" != "$expected_checksum" ]; then
+        echo "Checksum verification failed for $output_path! (expected: $expected_checksum, got: $actual_checksum)"
+        rm "$output_path"
+        exit 1
+    fi
+}
+
 # NPM: https://github.com/bluwy/npm-userscript
-curl -fsSL https://update.greasyfork.org/scripts/559139/Npm%20Userscript.user.js -o $DOTFILE_ROOT/userscripts/npm.user.js
+download_and_verify_file \
+    "https://update.greasyfork.org/scripts/559139/Npm%20Userscript.user.js" \
+    "$DOTFILE_ROOT/userscripts/npm.user.js" \
+    "2536a7b5679cf097b2d219588b750cff9dae6213028558e72e2ee8b2fc3ae53f"
