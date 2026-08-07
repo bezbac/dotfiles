5562c836ae648fba100792de4bffc651b453ccc0
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-08-07 16:59:24 +0200

    Avoid zsh slowdown from proto

--- zsh/plugins/proto.plugin.zsh
+++ zsh/plugins/proto.plugin.zsh
@@ -2,6 +2,9 @@
 
 export PROTO_HOME="$HOME/.proto"
 
+# Avoid refreshing Proto's cached WASM plugins on every directory change.
+export PROTO_CACHE_DURATION=31536000
+
 eval "$(proto activate zsh)"
 
 # TODO: Remove proto from path when leaving a proto project
