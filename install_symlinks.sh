d14dd4409e1f4a4ae14885c85459bf837689fd87
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-09-13 19:07:55 +0200

    Add sandbox sandbox plugin

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -15,6 +15,7 @@ mkdir -p $HOME/.config/zed
 mkdir -p $HOME/.zsh
 mkdir -p $HOME/.agents
 mkdir -p $HOME/.config/opencode/command
+mkdir -p $HOME/.config/opencode-sandbox
 
 # Create Symlinks
 ln -sfn $DOTFILE_ROOT/.editorconfig $HOME/.editorconfig
@@ -46,6 +47,7 @@ ln -sfn $DOTFILE_ROOT/opencode/plugins $HOME/.config/opencode/plugins
 ln -sfn $DOTFILE_ROOT/opencode/command $HOME/.config/opencode/command
 ln -sfn $DOTFILE_ROOT/opencode/skills $HOME/.config/opencode/skills
 ln -sfn $DOTFILE_ROOT/opencode/opencode-notifier.json $HOME/.config/opencode/opencode-notifier.json
+ln -sfn $DOTFILE_ROOT/opencode/sandbox-config.json $HOME/.config/opencode-sandbox/config.json
 ln -sfn $DOTFILE_ROOT/opencode/opencode-langfuse.json $HOME/.config/opencode/opencode-langfuse.json
 ln -sfn $DOTFILE_ROOT/opencode/acp.jsonc $HOME/.config/opencode/acp.jsonc
 ln -sfn $DOTFILE_ROOT/worktrunk/config.toml $HOME/.config/worktrunk/config.toml
